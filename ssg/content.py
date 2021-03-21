94476f987eb9fc3bc088eb957e8ea12148505ec2
Christopher Babu <chrisbabu123@gmail.com>
2021-03-21 20:02:28 +0530

    partial module 4

---
+++ ssg/content.py
@@ -0,0 +1,21 @@
+import re
+from yaml import FullLoader
+from collections.abc import Mapping
+
+class Content(Mapping):
+    __delimiter = r"^(?:-|\+){3}\s*$"
+    __regex = re.compile( __delimiter, re.MULTILINE)
+
+    @classmethod
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
+        metadata = load(fm, Loader = FullLoader)
+        return cls(metadata, content)
+
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data['content'] = content
+
+    @property
+    def body(self):
+        return self.data['content']
